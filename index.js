a466d34a113f583ab7092d3ee75728c79ea60069
MMJFrog <72291738+MMJFrog@users.noreply.github.com>
2020-11-09 10:14:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World1')
-  res.end('Hello world2')
+  res.send('Hello World')
 })
 
 app.listen(port, () => {
